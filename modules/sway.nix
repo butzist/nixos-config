fd2a740a1aa787af6a02725312e4730fbdab90b9
Adam Szalkowski <a.szalkowski@datahow.ch>
2024-07-03 10:32:17 +0200

    fix(sway): screen sharing

--- modules/sway.nix
+++ modules/sway.nix
@@ -9,26 +9,26 @@
 
   wayland.windowManager.sway.enable = true;
   wayland.windowManager.sway.package = null;
-  wayland.windowManager.sway.checkConfig = false;
+  wayland.windowManager.sway.checkConfig = true;
   wayland.windowManager.sway.config = {
     modifier = "Mod4";
     terminal = "/usr/bin/alacritty";
-    menu = "rofi -show drun -show-icons | xargs swaymsg exec --";
+    menu = "${pkgs.wofi}/bin/wofi -I --show drun | xargs swaymsg exec --";
 
     output = {
-      "Samsung Electric Company S27F350 H4ZMA14287" =  {
+      "Samsung Electric Company S27F350 H4ZMA14287" = {
         pos = "0 720";
         res = "1920x1080";
         scale = "1";
       };
 
       "Samsung Electric Company LS27A600U H4ZT400506" = {
-        pos  = "1920 360";
+        pos = "1920 360";
         res = "2560x1440";
         scale = "1";
-      }; 
+      };
 
-      eDP-1  = {
+      eDP-1 = {
         pos = "4480 0";
         res = "2880x1800";
         scale = "1.5";
@@ -45,51 +45,65 @@
 
       "*" = {
         xkb_options = "compose:menu";
-      } ;
+      };
     };
 
-    bars = [
-      { command = "waybar"; }
-    ];
+    bars = [];
 
     gaps = {
-      outer = 20;
+      outer = 15;
       inner = 10;
     };
   };
 
-  wayland.windowManager.sway.systemd.enable = false;
+  wayland.windowManager.sway.systemd.enable = true;
   wayland.windowManager.sway.wrapperFeatures.gtk = true;
-  wayland.windowManager.sway.extraConfig = ''
-  # Screen lock
-    set $lock_command swaylock --daemonize --ignore-empty-password --show-failed-attempts -c000000
-    exec swayidle -w \
-    timeout 5 'if pgrep -x swaylock; then swaymsg "output * dpms off"; fi' \
-    resume 'swaymsg "output * dpms on"' \
-    before-sleep "$lock_command"
+  wayland.windowManager.sway.extraConfig = let
+    script = pkgs.writeShellScript "import-gsettings.sh" ''
+      config="''${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
+      if [ ! -f "$config" ]; then exit 1; fi
 
-  # Apply GTK theme
-  exec_always ~/bin/import-gsettings
-    
-  include /etc/sway/config.d/*
+      gnome_schema="org.gnome.desktop.interface"
+      gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+      icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+      cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+      font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
+      gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
+      gsettings set "$gnome_schema" icon-theme "$icon_theme"
+      gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
+      gsettings set "$gnome_schema" font-name "$font_name"
+    '';
+  in ''
+    for_window [title="^.*"] border pixel 2, title_format "%title"
+
+    # Screen lock
+      set $lock_command /usr/bin/swaylock --daemonize --ignore-empty-password --show-failed-attempts -c000000
+      exec swayidle -w \
+      timeout 5 'if pgrep -x swaylock; then swaymsg "output * dpms off"; fi' \
+      resume 'swaymsg "output * dpms on"' \
+      before-sleep "$lock_command"
+
+    # Apply GTK theme
+    exec ${script}
   '';
-#    Lock after timeout
-#    timeout 300 'swaymsg "output * dpms off"' \
-#    timeout 305 "$lock_command" \
+  #    Lock after timeout
+  #    timeout 300 'swaymsg "output * dpms off"' \
+  #    timeout 305 "$lock_command" \
 
   wayland.windowManager.sway.config.keybindings = let
     modifier = config.wayland.windowManager.sway.config.modifier;
-  in lib.mkOptionDefault {
-    "${modifier}+l" = "exec $lock_command";
-    "${modifier}+Shift+e" = "exec wlogout";
-    Print = "exec grimshot copy anything";
-    XF86AudioRaiseVolume = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-    XF86AudioLowerVolume = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-    XF86AudioMute = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-    XF86AudioMicMute = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-    XF86MonBrightnessDown = "exec brightnessctl set 5%-";
-    XF86MonBrightnessUp = "exec brightnessctl set 5%+";
-  };
+  in
+    lib.mkOptionDefault {
+      "${modifier}+l" = "exec $lock_command";
+      "${modifier}+Shift+e" = "exec wlogout";
+      Print = "exec grimshot copy anything";
+      XF86AudioRaiseVolume = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
+      XF86AudioLowerVolume = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+      XF86AudioMute = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
+      XF86AudioMicMute = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+      XF86MonBrightnessDown = "exec brightnessctl set 5%-";
+      XF86MonBrightnessUp = "exec brightnessctl set 5%+";
+    };
 
   gtk = {
     enable = true;
@@ -99,8 +113,7 @@
     };
   };
 
+  programs.wofi.enable = true;
   programs.wlogout.enable = true;
-  programs.rofi.enable = true;
   programs.swaylock.enable = true;
 }
-
