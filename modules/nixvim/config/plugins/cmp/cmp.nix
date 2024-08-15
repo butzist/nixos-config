a7704c331e6443b8305adcfa4f7d27401588d742
Adam Szalkowski <a.szalkowski@datahow.ch>
2024-08-15 10:00:45 +0200

    fix(nvim): completion keymaps

--- modules/nixvim/config/plugins/cmp/cmp.nix
+++ modules/nixvim/config/plugins/cmp/cmp.nix
@@ -11,7 +11,7 @@
           fetchingTimeout = 200;
           maxViewEntries = 30;
         };
-        snippet = {expand = "luasnip";};
+        snippet.expand = ''function(args) require('luasnip').lsp_expand(args.body) end'';
         formatting = {fields = ["kind" "abbr" "menu"];};
         sources = [
           {name = "git";}
@@ -38,14 +38,15 @@
         };
 
         mapping = {
-          "<C-Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
           "<C-j>" = "cmp.mapping.select_next_item()";
           "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<DOWN>" = "cmp.mapping.select_next_item()";
+          "<UP>" = "cmp.mapping.select_prev_item()";
           "<C-e>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
-          "<C-CR>" = "cmp.mapping.confirm({ select = true })";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
         };
       };
