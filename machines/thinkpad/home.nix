efbd7fb091eb45364cd3c30c8ca42cbc0f4a44a7
Adam Szalkowski <a.szalkowski@datahow.ch>
2024-11-25 10:23:30 +0100

    fix(thinkpad): setup external screens

--- machines/thinkpad/home.nix
+++ machines/thinkpad/home.nix
@@ -1,7 +1,9 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1, preferred, auto-right, 1.5"
+      "eDP-1, preferred, 5120x240, 1.5"
+      "desc:Dell Inc. DELL P2720DC 3CPVL93, preferred, 0x0, 1"
+      "desc:Dell Inc. DELL P2720DC 30GSL93, preferred, 2560x0, 1"
     ];
   };
 
