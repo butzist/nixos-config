ae2f049e266f8c2c41a157b2b3affe361e0051ed
Adam Szalkowski <a.szalkowski@datahow.ch>
2025-01-27 16:09:51 +0100

    feat: revert back to using docker

--- modules/containers.nix
+++ modules/containers.nix
@@ -1,22 +1,16 @@
 {pkgs, ...}: {
-  # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
   virtualisation = {
-    podman = {
+    # Enable common container config files in /etc/containers
+    containers.enable = true;
+
+    docker = {
       enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      rootless.enable = false;
     };
   };
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    podman-compose # start group of containers for dev
   ];
 }
