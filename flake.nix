28e7c57852d76c9ff163f2fa7c57dd1177d8115f
Adam Szalkowski <a.szalkowski@datahow.ch>
2025-02-11 09:02:46 +0100

    chore: update (with temporary overlays)

--- flake.nix
+++ flake.nix
@@ -6,14 +6,18 @@
     nixpkgs-bleeding.url = "github:nixos/nixpkgs/master";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     nvf.url = "github:notashelf/nvf";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {nixpkgs, ...}: let
     system = "x86_64-linux";
-    overlay-nixpkgs = final: _last: {
+    overlay-nixpkgs = final: prev: {
       stable = import inputs.nixpkgs-stable {
         inherit system;
         config.allowUnfree = true;
@@ -21,6 +25,12 @@
       bleeding = import inputs.nixpkgs-bleeding {
         inherit system;
         config.allowUnfree = true;
+      };
+      basedpyright = prev.basedpyright.overrideAttrs {
+        dontCheckForBrokenSymlinks = true;
+      };
+      lldb = prev.lldb.overrideAttrs {
+        dontCheckForBrokenSymlinks = true;
       };
     };
   in {
