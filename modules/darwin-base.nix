cbb7fe9d8f8df02bfc6c127faf997a18f0f2d8fc
Adam Szalkowski <a.szalkowski@datahow.ch>
2025-02-26 07:33:41 +0100

    chore: update

--- modules/darwin-base.nix
+++ modules/darwin-base.nix
@@ -19,7 +19,7 @@
     ];
   };
 
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   system.keyboard = {
     enableKeyMapping = true;
     remapCapsLockToEscape = true;
