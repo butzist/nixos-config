975ee77ecb3fb31349e3702c6bede95e579e0801
Adam Szalkowski <a.szalkowski@datahow.ch>
2025-03-12 08:10:37 +0100

    chore: update

--- modules/home/work/vscode.nix
+++ modules/home/work/vscode.nix
@@ -20,7 +20,7 @@
           usernamehw.errorlens
           vscodevim.vim
           dbaeumer.vscode-eslint
-          pkgs.bleeding.vscode-extensions.ms-python.python
+          ms-python.python
           ms-python.pylint
           ms-python.black-formatter
         ]
