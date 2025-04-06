0691d8859cb1a2b9d10a768bfc88a3e7332f255d
Adam M. Szalkowski <adam@szalkowski.de>
2025-04-06 15:11:06 +0200

    chore: update

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -41,7 +41,6 @@
         trouble.enable = true;
         lspSignature.enable = true;
         otter-nvim.enable = true;
-        lsplines.enable = true;
         nvim-docs-view.enable = true;
       };
 
