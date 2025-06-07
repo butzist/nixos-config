083de873c0573a9f77b86691340fbf264de5d1bf
Adam M. Szalkowski <adam@szalkowski.de>
2025-06-07 14:01:54 +0200

    feat: enable gpg agent

--- modules/system/nixos/base.nix
+++ modules/system/nixos/base.nix
@@ -124,6 +124,12 @@
     };
   };
 
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
