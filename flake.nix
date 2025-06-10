6229c2301c303951eb047ef268d6a81426d2425e
Adam Szalkowski <a.szalkowski@datahow.ch>
2025-06-10 14:06:55 +0200

    chore(stylix): update repo

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:nix-community/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
