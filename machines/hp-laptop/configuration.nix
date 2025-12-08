0ad8c5815de7748cdcb59a30946ce5710182f330
Adam M. Szalkowski <adam@szalkowski.de>
2025-12-08 08:17:38 +0100

    feat(users+machines): add new laptop

---
+++ machines/hp-laptop/configuration.nix
@@ -0,0 +1,76 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{pkgs, ...}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/system/nixos/base.nix
+    ../../modules/system/nixos/gnome.nix
+    ../../modules/system/nixos/gaming.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "hp-laptop"; # Define your hostname.
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "nodeadkeys";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.adam = {
+    isNormalUser = true;
+    description = "Adam";
+    extraGroups = ["networkmanager" "wheel" "docker" "video"];
+    uid = 1000;
+    shell = pkgs.bash;
+  };
+
+  users.users.games = {
+    isNormalUser = true;
+    description = "Gaming";
+    extraGroups = ["networkmanager" "video" "gamemode"];
+    uid = 1002;
+    shell = pkgs.bash;
+  };
+
+  users.users.mirj = {
+    isNormalUser = true;
+    description = "Mirjam";
+    extraGroups = ["networkmanager" "video"];
+    uid = 1003;
+    shell = pkgs.bash;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
---
+++ users/mirj.nix
@@ -0,0 +1,42 @@
+{pkgs, ...}: let
+in {
+  home.username = "mirj";
+  home.homeDirectory = "/home/mirj";
+
+  imports = [
+    ../modules/home/base.nix
+    ../modules/home/nushell.nix
+    ../themes/ubuntu-catppuccin.nix
+  ];
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    security.brave
+    security.firefox
+    freecad
+    inkscape
+    orca-slicer
+    gimp3-with-plugins
+    libreoffice
+    hunspell
+    hunspellDicts.de_CH
+    hunspellDicts.de_DE
+    hunspellDicts.en_US
+  ];
+
+  programs = {
+    bash = {
+      enable = true;
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+}
