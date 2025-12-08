86c3e7e6796c8e4eeeb8de1fa2f799015e5d06bb
Adam M. Szalkowski <adam@szalkowski.de>
2025-12-08 20:05:55 +0100

    feat(system): add fwupd

--- modules/system/nixos/base.nix
+++ modules/system/nixos/base.nix
@@ -133,6 +133,8 @@
     enableSSHSupport = true;
   };
 
+  services.fwupd.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
